80e9da0015a8597048f6810f840fd160c4c0bde6
soohyeon <su132489@gmail.com>
2023-08-24 22:35:56 +0900

    Feat: 출력 debounce 시간 줄임

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const printFontEl = document.getElementById('printFont');
 const debounce = new Debounce();
 
 textEl.addEventListener('keyup', () => {
-  debounce.setTimer(() => {printFont(textEl.value, printFontEl)});
+  debounce.setTimer(() => {printFont(textEl.value, printFontEl)}, 100);
 })
 
 
